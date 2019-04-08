a0f767d5f010eb312be57b408ae9943873d939fd
Marco Vidal García <marcovidal67@gmail.com>
2019-04-08 23:19:10 +0200

    Use url lib to assemble request url

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const request = require('request');
+const {URL, URLSearchParams} = require('url');
 
-const url = 'https://api.skypicker.com/flights?fly_from=GVA&fly_to=KEF&date_from=17/04/2019&date_to=27/04/2019&return_from=27/04/2019&return_to=27/04/2019&partner=picky&sort=quality&price_to=290'
-const url2 = 'https://api.skypicker.com/flights?fly_from=GVA&fly_to=LHR&date_from=21/11/2018&date_to=26/11/2018&partner=picky&sort=quality'
+const API_URL = new URL('https://api.skypicker.com/flights');
+const params = {
+  fly_from: 'GVA',
+  fly_to: 'BRU',
+  date_from: '01/05/2019',
+  date_to: '01/05/2019',
+  return_from: '02/05/2019',
+  return_to: '02/05/2019',
+  partner: 'picky',
+  sort: 'quality',
+  price_to: 290,
+};
+let url = API_URL;
+url.search = new URLSearchParams(params);
+url = url.href;
+console.log(url);
 
-request(url, { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
+request(url, {json: true}, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
   console.log(body);
 });
