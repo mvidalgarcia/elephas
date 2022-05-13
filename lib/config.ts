ca582a98adaf90da3f9f2df971579423c017eb89
Marco Vidal García <marcovidal67@gmail.com>
2022-05-13 22:03:24 +0200

    feat(search): support multiple destination search
    - user must provide an array as `fly_to` param instead of a string
    
    closes #7

--- lib/config.ts
+++ lib/config.ts
@@ -1,9 +1,9 @@
 export const TEQUILA_API_URL: string = "https://tequila-api.kiwi.com/v2/search";
 
 // FIXME: Hardcoded user config to make customizable
-export const USER_SEARCH_PARAMS = {
+export const USER_SEARCH_PARAMS: SearchProps = {
   fly_from: "BCN",
-  fly_to: "MAD",
+  fly_to: ["MAD", "GVA", "OVD", "IBZ", "CAG"],
   dtime_from: "15:00",
   ret_dtime_from: "16:00",
   partner: "picky",
@@ -12,4 +12,17 @@ export const USER_SEARCH_PARAMS = {
   price_to: "200",
   curr: "EUR",
   max_stopovers: "0",
+};
+
+type SearchProps = {
+  fly_from: string;
+  fly_to: Array<string>;
+  dtime_from: string;
+  ret_dtime_from: string;
+  partner: string;
+  sort: string;
+  price_from: string;
+  price_to: string;
+  curr: string;
+  max_stopovers: string;
 };
--- lib/search.ts
+++ lib/search.ts
@@ -8,41 +8,27 @@ dotenv.config();
 
 const TEQUILA_API_KEY: string = String(process.env.TEQUILA_API_KEY);
 
-type SearchProps = {
-  fly_from: string;
-  fly_to: string;
-  date_from: string;
-  date_to: string;
-  return_from: string;
-  return_to: string;
-  dtime_from: string;
-  ret_dtime_from: string;
-  partner: string;
-  sort: string;
-  price_from: string;
-  price_to: string;
-  curr: string;
-  max_stopovers: string;
-};
-
 export function search(
   searchParams: typeof USER_SEARCH_PARAMS,
   weekendCount: number = 5
 ) {
   const weekends = getNextWeekends({ count: weekendCount });
 
-  const urls = weekends.map(({ friday, sunday }) => {
-    const fullSearchParams = {
-      ...searchParams,
-      date_from: friday,
-      date_to: friday,
-      return_from: sunday,
-      return_to: sunday,
-    };
-    const urlObj = new URL(TEQUILA_API_URL);
-    urlObj.search = new URLSearchParams(fullSearchParams).toString();
-    const urlStr = urlObj.href;
-    return urlStr;
+  const urls = searchParams.fly_to.flatMap((to) => {
+    return weekends.map(({ friday, sunday }) => {
+      const fullSearchParams = {
+        ...searchParams,
+        date_from: friday,
+        date_to: friday,
+        return_from: sunday,
+        return_to: sunday,
+        fly_to: to,
+      };
+      const urlObj = new URL(TEQUILA_API_URL);
+      urlObj.search = new URLSearchParams(fullSearchParams).toString();
+      const urlStr = urlObj.href;
+      return urlStr;
+    });
   });
 
   return Promise.all(
